4bc6d9964269336c50a2c068b72b179e5f12384a
13ankyroy <34185591+13ankyroy@users.noreply.github.com>
2023-06-22 14:27:54 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Squirrel;
+
+namespace SampleConsole
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            CheckForUpdates();
+
+        }
+
+        static async void CheckForUpdates()
+        {
+            using (var manager = await UpdateManager.GitHubUpdateManager(@"https://github.com/meJevin/WPFCoreTest"))
+            {
+                await manager.UpdateApp();
+            }
+        }
+    }
+}
